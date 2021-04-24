265c3ff4a41dfc25f7d7d3fd796723cdbbee5f05
KyungBin7 <jkb12377@naver.com>
2021-04-24 16:14:23 +0900

    Deque

---
+++ Datastructures/Deque/Deque/Deque.h
@@ -0,0 +1,49 @@
+﻿#include "Queue.h"
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#define MAX_QUEUE_SIZE 100
+typedef int itemtype;
+class Deque : public Queue {
+public:
+	Deque() {}
+	void addRear(itemtype item) { enqueue(item); }
+	itemtype deleteFront() { return dequeue(); }
+	itemtype gerFront() { return peek(); }
+	void addFront(itemtype item) {
+		if (is_full()) {
+			cout << "덱이 포화상태입니다\n";
+			exit(1);
+		}
+		else {
+			data[front] = item;
+			front = (front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+		}
+	}
+	itemtype deleteRear() {
+		if (is_empty()) {
+			cout << "공백상태입니다\n";
+			exit(1);
+		}
+		else {
+			itemtype ret = data[rear];
+			rear = (rear -1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+			return ret;
+		}
+	}
+	itemtype getRear() {
+		if (is_empty()) {
+		cout << "공백입니다\n";
+		exit(1);
+		}
+		else 
+			return data[rear];
+	}
+	void display() {
+		cout << "덱의 내용 : \n"; // 이 출력 코드만 다름
+		int length = (rear - front > 0) ? rear - front : (rear - front) + MAX_QUEUE_SIZE;
+		for (int i = front + 1; i <= front + 1 + length; i++)
+			cout << "< " << data[i % MAX_QUEUE_SIZE] << " > ";
+		cout << "\n";
+	}
+};
---
+++ Datastructures/Deque/Deque/Queue.cpp
@@ -0,0 +1,51 @@
+#include "Queue.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+Queue::Queue()
+{
+	front = rear = 0;
+}
+bool Queue::is_empty() {
+	return front == rear;
+}
+bool Queue::is_full() {
+	return front == (rear + 1) % MAX_QUEUE_SIZE;
+}
+void Queue::enqueue(itemtype item) {
+	if (is_full()) {
+		cout << "큐가 포화상태입니다\n";
+		exit(1);
+	}
+	else {
+		rear = (rear + 1) % MAX_QUEUE_SIZE;
+		data[rear] = item;
+	}
+}
+itemtype Queue::dequeue() {
+	if (is_empty()) {
+		cout << "빈큐입니다\n";
+		exit(1);
+	}
+	else {
+		front = (front + 1) % MAX_QUEUE_SIZE;
+		return data[front];
+	}
+}
+itemtype Queue::peek() {
+	if (is_empty()) {
+		cout << "빈큐입니다\n";
+		exit(1);
+	}
+	else
+		return data[(front + 1) % MAX_QUEUE_SIZE];
+
+}
+void Queue::display() {
+	cout << "큐내용 : ";
+	int length = (rear - front > 0) ? rear - front : (rear - front) + MAX_QUEUE_SIZE;
+	for (int i = front + 1; i < front + 1 + length; i++)
+		cout << "<" << data[i % MAX_QUEUE_SIZE] << ">";
+	cout << "\n";
+}
